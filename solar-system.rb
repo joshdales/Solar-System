8bad10e9cc1092b271d9f84080850324d607ed6b
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-28 15:23:18 -0400

    Create sub-classes within Body for Planet, Moon, and Star

--- solar-system.rb
+++ solar-system.rb
@@ -1,6 +1,34 @@
 class System
 @@bodies = []
 
+def bodies
+  @@bodies
+end
 
+def add
+  new_body =
+  @@bodies << new_body
+  new_body
+end
+
+end
+
+
+class Body
+  def initialize(name, size)
+    @name = name
+    @size = size
+  end
+end
+
+class Planet < Body
+
+end
+
+class Star < Body
+
+end
+
+class Moon < Body
 
 end
